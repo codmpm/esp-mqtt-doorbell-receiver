8577c378c31cd035c3d424c12e0b4f835861c49e
Patrik Mayer <patrik.mayer@codm.de>
2017-03-27 22:16:22 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,58 @@
+# 433MHz ESP MQTT Doorbell Receiver
+
+I wanted to be able to get noticed when our doorbell rang. It is a cheap radio doorbell labeled QH-C-CE, which I ordered from amazon years ago. 
+After opening it up I was lucky to find a 433MHz crystal and thought it must be possible to receive a radio packet from it.
+
+So I ordered some cheap [433MHz modules](https://www.amazon.de/XCSOURCE-Transmitter-Receiver-Arduino-TE122/dp/B00V4ISS38/ref=sr_1_3?ie=UTF8&qid=1490643731&sr=8-3) and started to investigate. Luckily there is already a versatile library to receive 433MHz-signal: [rc-switch](https://github.com/sui77/rc-switch) - of course this should also work with 315MHz devices.
+
+I've used the `receive-simple` example and got a very clear reception from my doorbell button. Also I got the radio code for my particular sender. Then it was quite simple to write a basic sketch which sends an mqtt message everytime the doorbell button gets pressed.
+
+## Get it working
+This needs the mentioned 433MHz-Receiver Modules and is designed for an ESP8266. I my case I've used an ESP-01 as I need only need one GPIO.
+
+The receiver should be fitted with a 17cm solid core wire as antenna and _must_ be supplied with 5V as the reception is very poor if you drive it with 3.3V.
+
+The ESP-01 needs 3.3V so you need to level-shift the output of the receiver module (5V) before feeding it into the ESP. I've used a voltage divider (20kΩ/10kΩ) which is not the best way but worked very well. The output is wired to `GPIO2` of the ESP.
+
+![image of breadboard wiring](esp-mqtt-doorbell-receiver-breadboard.jpg)
+
+	TODO: Add circuit schematic
+
+After the first upload of the sketch, you could update the ESP via [ArduinoOTA](http://esp8266.github.io/Arduino/versions/2.3.0/doc/ota_updates/readme.html). The OTA name is set to the `mqttClienName` which is also used for the ESP-hostname. After an OTA update the ESP gets restarted.
+
+## Functions
+The sketch simply listens for a radio packet with the configured code. On receiving it publishes a message to an mqtt-broker to `<topic-prefix>ring`, non persistent.
+
+After receiving a valid radio code it would not receive any more packets for the next 5 seconds (configrable). This is cause the most doorbell buttons will transmit at least 5-8 packets or as long as the button is pressed.
+
+Additionaly the online status and the IP of the device will be published to `<topic-prefix>status` and `<topic-prefix>ip`. The status is done via mqtt's last-will statement and will state `offline` if the device disconnects for some reason.
+
+I use node-red to send me a push notification with pushover everytime the doorbel is rang. Quite simple:
+![image of node-red wiring for pushover](esp-mqtt-doorbell-receiver-node-red.png)
+
+## Config
+Change the settings at the top of the .ino-file corresponding to your needs. Ensure that you choose a unique mqtt client id. If you do not need broker-credentials just set user and password to an empty string.
+
+You will have to use the receive-example demo to get your specific radio code for your doorbell button.
+
+The serial console is left open to debug and check the WiFi connection. Baudrate 115200, 8N1.
+
+Created with
+- Arduino 1.8.1 (https://www.arduino.cc/)
+- ESP8266 board definition 2.3.0 (https://github.com/esp8266/Arduino)
+- PubSubClient 2.6.0 by Nick O'Leary (https://github.com/knolleary/pubsubclient)
+- Arduino OTA 2.3.0 http://esp8266.github.io/Arduino/versions/2.3.0/doc/ota_updates/readme.html
+
+### Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+### Credits
+Patrik Mayer, 2017 
+
+### License
+MIT
---
+++ esp-mqtt-doorbell-receiver/esp-mqtt-doorbell-receiver.ino
@@ -0,0 +1,207 @@
+/*
+  mqtt-doorbell-receiver (c) 2017 patrik.mayer@codm.de
+
+  This is a simple sketch to publish to mqtt when the doorbell rings
+  Based on https://github.com/sui77/rc-switch/
+
+  Decimal 3954437, Protocol 1
+
+*/
+
+#include <RCSwitch.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+
+//---- config
+
+// WiFi
+const char* ssid = "<your-wifi-ssid>";
+const char* password = "<your-wifi-key>";
+
+const char* mqttServer = "<mqtt-broker-ip-or-host>";
+const char* mqttUser = "<mqtt-user>";
+const char* mqttPass = "<mqtt-password>";
+const char* mqttClientName = "<mqtt-client-id>"; //will also be used hostname and OTA name
+const char* mqttTopicPrefix = "<mqtt-topic-prefix>";
+
+//doorbell - use rc-switch receive example to get the code
+long wantedCodeDec = 3954437;
+uint8_t wantedProt = 1;
+long ringDelay = 5000;
+
+//----
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+RCSwitch mySwitch = RCSwitch();
+
+char mqttTopicStatus[64];
+char mqttTopicIp[64];
+char mqttTopic[64];
+
+long lastReconnectAttempt = 0; //For the non blocking mqtt reconnect (in millis)
+long lastRing = 1;
+
+void setup() {
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqttServer, 1883);
+
+  //put in mqtt prefix
+  sprintf(mqttTopicStatus, "%sstatus", mqttTopicPrefix);
+  sprintf(mqttTopicIp, "%sip", mqttTopicPrefix);
+  sprintf(mqttTopic, "%sring", mqttTopicPrefix);
+
+  //rc receiver
+  mySwitch.enableReceive(2);  // Using GPIO2 on ESP-01
+
+  //----------- OTA
+  ArduinoOTA.setHostname(mqttClientName);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+    delay(1000);
+    ESP.restart();
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+  Serial.println("ready...");
+
+}
+
+void loop() {
+
+  //handle mqtt connection, non-blocking
+  if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (MqttReconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  }
+  client.loop();
+  
+
+  if (mySwitch.available()) {
+    if (lastRing == 0) {
+      int value = mySwitch.getReceivedValue();
+      if (value > 0) {
+
+        if (
+          mySwitch.getReceivedValue() == wantedCodeDec &&
+          mySwitch.getReceivedProtocol() == wantedProt
+        ) {
+          Serial.print("Riiing");
+          Serial.print(" : ");
+          Serial.print("Received ");
+          Serial.print( mySwitch.getReceivedValue() );
+          Serial.print(" / ");
+          Serial.print( mySwitch.getReceivedBitlength() );
+          Serial.print("bit ");
+          Serial.print("Protocol: ");
+          Serial.println( mySwitch.getReceivedProtocol() );
+
+          lastRing = millis();
+          client.publish(mqttTopic, "1");
+
+        }
+      }
+    }
+    mySwitch.resetAvailable();
+  }
+
+  //reset lastRing time if the ringDelay is reached
+  //also reset ringCtr as there could be too few packets from last receive
+  if ((lastRing > 0) && ((millis() - lastRing) > ringDelay)) {
+    lastRing = 0;
+  }
+
+  //handle OTA
+  ArduinoOTA.handle();
+
+}
+
+
+void setup_wifi() {
+
+  delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA); //disable AP mode, only station
+  WiFi.hostname(mqttClientName);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+bool MqttReconnect() {
+
+  if (!client.connected()) {
+
+    Serial.print("Attempting MQTT connection...");
+
+    // Attempt to connect with last will retained
+    if (client.connect(mqttClientName, mqttUser, mqttPass, mqttTopicStatus, 1, true, "offline")) {
+
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      char curIp[16];
+      sprintf(curIp, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
+
+      client.publish(mqttTopicStatus, "online", true);
+      client.publish(mqttTopicIp, curIp, true);
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+    }
+  }
+  return client.connected();
+}
+
+
